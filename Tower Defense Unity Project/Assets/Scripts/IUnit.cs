79c14c27fcc31a3aaaaf4160c594ca42536bf982
d-theodorakopoulos <d.theodorakopoulos@outlook.com>
2017-11-05 18:48:37 +0200

    Add IUnit interface

---
+++ Tower Defense Unity Project/Assets/Scripts/IUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public interface IUnit
+    {
+        void Spaw (Vector3 position);
+        void Die ();
+        void TakeItem (object item);
+        void TakeDamage (float amount);
+    }
+}
